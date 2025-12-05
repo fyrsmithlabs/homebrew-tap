04de6c2298ef7fda2abd5a296888927e5be66190
Dustin <11720061+dahendel@users.noreply.github.com>
2025-12-05 13:02:42 -0600

    feat: build from source with CGO_ENABLED=1

--- Formula/contextd.rb
+++ Formula/contextd.rb
@@ -7,52 +7,26 @@ class Contextd < Formula
   version "0.2.0-alpha"
   license "MIT"
 
+  url "https://github.com/fyrsmithlabs/contextd/archive/refs/tags/v0.2.0-alpha.tar.gz"
+  sha256 "94beca968d23b780f40e1688f70a5817688952cd40b13dae5c6ab20811b6c28a"
+
+  depends_on "go" => :build
   depends_on "onnxruntime"
 
-  on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/fyrsmithlabs/contextd/releases/download/v0.2.0-alpha/contextd_0.2.0-alpha_darwin_amd64.tar.gz"
-      sha256 "9a1ee9a4ad456d7a5b608f426020331306cfeaafb17c4edc88ba8a842178bddb"
+  def install
+    ENV["CGO_ENABLED"] = "1"
+    
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+    ]
 
-      def install
-        bin.install "contextd"
-        bin.install "ctxd"
-      end
-    end
-    if Hardware::CPU.arm?
-      url "https://github.com/fyrsmithlabs/contextd/releases/download/v0.2.0-alpha/contextd_0.2.0-alpha_darwin_arm64.tar.gz"
-      sha256 "7a362e65628c5d4c4776dcce1999749306bcaefd682923d2405edc7aaee50acc"
-
-      def install
-        bin.install "contextd"
-        bin.install "ctxd"
-      end
-    end
-  end
-
-  on_linux do
-    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/fyrsmithlabs/contextd/releases/download/v0.2.0-alpha/contextd_0.2.0-alpha_linux_amd64.tar.gz"
-      sha256 "5011db9a61f3a05b22abad9ed6cb0be4799bdc2022d8416c57c74535b790360e"
-
-      def install
-        bin.install "contextd"
-        bin.install "ctxd"
-      end
-    end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/fyrsmithlabs/contextd/releases/download/v0.2.0-alpha/contextd_0.2.0-alpha_linux_arm64.tar.gz"
-      sha256 "fb04af6333d5904d643c25541fbcea2c96e305a96a63aa9a35349f215bb750da"
-
-      def install
-        bin.install "contextd"
-        bin.install "ctxd"
-      end
-    end
+    system "go", "build", *std_go_args(ldflags:), "-o", bin/"contextd", "./cmd/contextd"
+    system "go", "build", *std_go_args(ldflags:), "-o", bin/"ctxd", "./cmd/ctxd"
   end
 
   test do
-    system "#{bin}/contextd", "--help"
-    system "#{bin}/ctxd", "--help"
+    assert_match version.to_s, shell_output("#{bin}/contextd --version")
+    assert_match version.to_s, shell_output("#{bin}/ctxd --version")
   end
 end
